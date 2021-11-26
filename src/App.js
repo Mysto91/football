52e2c1abaf536487b3096053a74b70d43f1171c9
Etienne TRAN <etiennetran@hotmail.fr>
2021-11-26 17:02:30 +0100

    Début

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+const SerpApi = require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch("b4b0addf2eb0ce17d1bc6ba7eb3fdc91f501069c34c727e5508af2af2f3b8861");
+
+const params = {
+  q: "Olympique de Marseille",
+  location: "austin, texas, united states"
+};
+
+const callback = function (data) {
+  console.log(data['sports_results']);
+};
+
+// Show result as JSON
+search.json(params, callback);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+      </div>
     </div>
   );
 }
